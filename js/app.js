19a8a3bcf39ce7ccea070819b109ed8ee7988db4
jozuah <joshua.galbas@gmail.com>
2021-06-02 10:18:00 +0200

    Add desktop button with hover

--- js/app.js
+++ js/app.js
@@ -1,10 +1,17 @@
-const inOutNavbar = () => {
-    const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.nav-menu');
+// const inOutNavbar = () => {
+//     const burger = document.querySelector('.burger');
+//     const nav = document.querySelector('.nav-menu');
 
-    burger.addEventListener('click', () => {
-        nav.classList.toggle('.nav-active')
-    })
-}
+//     burger.addEventListener('click', () => {
+//         nav.classList.toggle('.nav-active')
+//     })
+// }
 
-inOutNavbar()
+// inOutNavbar()
+
+const $burger = document.querySelector('.burger')
+const $nav = document.querySelector('.nav-menu');
+
+$burger.addEventListener('click', () => {
+    $nav.classList.toggle('nav-active')
+})
