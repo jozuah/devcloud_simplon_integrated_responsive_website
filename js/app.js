f60676e95dff83606eebac1f6d9530e4fe39e4c8
jozuah <joshua.galbas@gmail.com>
2021-06-04 14:59:39 +0200

    sign up valid modal page

--- js/app.js
+++ js/app.js
@@ -52,3 +52,52 @@ span.onclick = function() {
 $('input[type="checkbox"]').on('change', function() {
     $('input[type="checkbox"]').not(this).prop('checked', false);
  });
+
+
+ /************* VALIDATION DU FORMULAIRE **********************/
+
+function validateForm() {
+  let first_name = document.forms["myForm"]["FirstName"].value;
+  if (first_name == "" || first_name.length <= 2) {
+    document.getElementById("errorMessageFirstName").innerHTML = "First name must contain atleast 2 characters";
+    return false;
+  }
+  
+  let last_name = document.forms["myForm"]["LastName"].value;
+  if (last_name == "" || last_name.length <= 2) {
+    document.getElementById("errorMessageLastName").innerHTML = "Last name must contain atleast 2 characters";
+    return false;
+  }
+
+  let email = document.forms["myForm"]["Email"].value;
+  // L'adresse mail ne doit pas etre vide et ontenir un @
+  if (email == "" || !email.includes("@")) {
+    document.getElementById("errorMessageEmail").innerHTML = "Please enter a valid email adress";
+    return false;
+  }
+
+  let birthdate = document.forms["myForm"]["Birthdate"].value;
+  if (birthdate == "") {
+    document.getElementById("errorMessageBirthdate").innerHTML = "Please enter a birthdate ";
+    return false;
+  }
+
+  let tournaments = document.forms["myForm"]["Tournaments"].value;
+  if (tournaments == "") {
+    document.getElementById("errorMessageTournaments").innerHTML = "Please enter a number ";
+    return false;
+  }
+
+  let terms = document.getElementById("Terms").checked;
+  if (terms==false)
+  {
+    document.getElementById("errorMessageTerms").innerHTML = "You must agree to the terms first.";
+      /*alert('You must agree to the terms first.');*/
+    return false;
+  }
+
+  else {
+    return true;
+  }
+}
+
